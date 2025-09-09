cd79d651dbc1b80f49f2e7a45205ef05e05603fe
linuxtusX5 <salvatusjoffrey@gmail.com>
2025-09-09 17:49:24 +0800

    Serializers v1

---
+++ backend/api/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import User, Item
+
+class UserSerializer(DocumentSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        # extra_kwargs = {'password': {'write_only': True}}
+
+class RegisterSerializer(DocumentSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+class ItemSerializer(DocumentSerializer):
+    class Meta:
+        model = Item
+        fields = ['id', 'name', 'description', 'created_at']
