f03f1044d1ab98dada3e86de59eab049883433e5
linuxtusX5 <salvatusjoffrey@gmail.com>
2025-09-09 18:05:08 +0800

    Item View set

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from .models import User, Item
+from .serializers import ItemSerializer
 
-# Create your views here.
+class ItemViewSet(viewsets.ModelViewSet):
+    serializer_class = ItemSerializer
+    permissions_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user_id = self.request.user.id
+        return Item.objects.filter(created_at=user_id)
+
+    def perform_create(self, serializer):
+        user = User.objects.get(id=self.request.user.id)
+        serializer.save(created_at=user)
