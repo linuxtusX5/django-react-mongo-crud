8f793da6325a5a951545e727faa984ce0b9d0c65
linuxtusX5 <salvatusjoffrey@gmail.com>
2025-09-10 18:03:32 +0800

    FE Setup v1

---
+++ frontend/src/pages/CRUD.tsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: process.env.VITE_BE_API_URL,
+});
+
+export const register = (data: {
+  username: string;
+  email: string;
+  password: string;
+}) => API.post("/register/", data);
+export const login = (data: { username: string; password: string }) =>
+  API.post("/login/", data);
+export const fetchItems = (token: string) =>
+  API.get("/items/", { headers: { Authorization: `Bearer ${token}` } });
+export const createItem = (
+  token: string,
+  data: { name: string; description: string }
+) =>
+  API.post("/items/", data, { headers: { Authorization: `Bearer ${token}` } });
